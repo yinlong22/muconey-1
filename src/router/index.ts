dee1ae15dbce42aca068e831b58bc6a21737d938
yinlong22 <342241857@.com>
2020-04-25 18:18:21 +0800

    做好了Nav栏

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,39 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import VueRouter, {RouteConfig} from 'vue-router'
+import Money from '@/views/Money.vue'
+import Labels from '@/views/Labels.vue'
+import Statistics from '@/views/Statistics.vue'
+import NotFound from '@/views/NotFound.vue'
 
 Vue.use(VueRouter)
 
-  const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+// @ts-ignore
+// @ts-ignore
+const routes: Array<RouteConfig> = [
+    {
+        path: '/',
+        redirect: 'money'//首页默认为money
+    },
+    {
+        path: '/money',
+        component: Money
+    },
+    {
+        path: '/labels',
+        component: Labels
+    },
+    {
+        path: '/statistics',
+        component: Statistics
+    },
+    {
+        path: '*',
+        component:NotFound
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
