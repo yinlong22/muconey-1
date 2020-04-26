4160269add57c2967783d5224822e2db5e8a1e26
yinlong22 <342241857@.com>
2020-04-26 14:22:50 +0800

    完成了点击type切换的功能

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
     <div>
         <Layout class-prefix="layout">
             <NumberPad/>
-            <Types/>
+            <Types xxx="hi"/>
             <Notes/>
             <Tags/>
         </Layout>
