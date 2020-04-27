0b5c00e366268dc9c2b657fd4f301fcbeb3ce63c
yinlong22 <342241857@.com>
2020-04-27 15:22:29 +0800

    封装出一个model

---
+++ src/custom.d.ts
@@ -0,0 +1,7 @@
+type RecordItem = {//type相当于js里的var
+    tags: string[];
+    notes: string;
+    type: string;
+    amount: number; //数据类型
+    createdAt?: Date; //类
+}
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,32 +16,28 @@
     import Notes from '@/components/Money/Notes.vue'
     import Tags from '@/components/Money/Tags.vue'
     import {Component, Watch} from 'vue-property-decorator'
+    import model from '@/model'
+
+    const recordList = model.fetch()
+    // const model = require('@/model.js').model
 
     const version = window.localStorage.getItem('version') || '0'
-    const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]')
     if (version === '0.0.1') {//数据库升级，数据迁徙
         recordList.forEach(record => {
             record.createdAt = new Date(2020, 0, 1)
         })
         window.localStorage.setItem('recordList', JSON.stringify(recordList))// 保存数据
-    }else
-    window.localStorage.setItem('version', '0.0.2')
+    } else
+        window.localStorage.setItem('version', '0.0.2')
     // 重置数据库版本
-    type Record = {//type相当于js里的var
-        tags: string[];
-        notes: string;
-        type: string;
-        amount: number; //数据类型
-        createdAt?: Date; //类
-    }
 
     @Component({
         components: {Tags, Notes, Types, NumberPad}
     })
     export default class Money extends Vue {
         tags = ['衣', '食', '住', '行', '吃鸡']
-        recordList: Record[] = recordList
-        record: Record = {tags: [], notes: '', type: '-', amount: 0}
+        recordList: RecordItem[] = recordList
+        record: RecordItem = {tags: [], notes: '', type: '-', amount: 0}
 
         // components:{Nav} /已通过全局引入(在main.ts里)
         onUpdateTags(value: string[]) {
@@ -54,14 +50,14 @@
 
         saveRecord() {
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            const record2: Record = JSON.parse(JSON.stringify(this.record))
+            const record2: RecordItem = model.clone(this.record)
             record2.createdAt = new Date()
             this.recordList.push(record2)
         }
 
         @Watch('recordList')
         onRecordListChanged() {
-            window.localStorage.setItem('recordList', JSON.stringify(this.recordList))
+            model.save(this.recordList)
         }
     }
 </script>
