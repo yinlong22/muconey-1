b3fe3675900be7c787dc50fd1b3d75b03f38ae02
yinlong22 <342241857@.com>
2020-05-03 11:29:00 +0800

    保证用户用手机打开页面，否则就提示用户

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,20 @@ new Vue({
     render: h => h(App)
 }).$mount('#app')
 
-window.onload=function () {
+window.onload = function () {
     setTimeout(function () {
-        window.scrollTo(0,9999)
-    },0)
+        window.scrollTo(0, 9999)
+    }, 0)
+}
+
+if (document.documentElement.clientWidth > 500) {
+    window.alert('请使用手机打开，以保证浏览效果')
+    const img = document.createElement('img')
+    img.src = '/qrcode.png'
+    img.style.position = 'fixed'
+    img.style.top = '26%'
+    img.style.left = '50%'
+    img.style.transform = 'translate(-50%,-50%)'
+    img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)'
+    document.body.appendChild(img)
 }
